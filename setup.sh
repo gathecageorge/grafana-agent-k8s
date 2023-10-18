c041190d22798b85660eb1fa8975fdcb06f01cfc
George Gatheca <gatheca.george@gmail.com>
2023-10-18 23:33:56 +0100

    Add scripts

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+kubectl create configmap org-loki-config --from-file org-loki.yml
+kubectl create configmap org-prom-config --from-file org-prom.yml
+kubectl create configmap prometheus-config --from-file prometheus.yml
+kubectl create configmap promtail-config --from-file promtail.yml
+kubectl create configmap entrypoint-config --from-file entrypoint.sh
+
+kubectl apply -f main.yml
