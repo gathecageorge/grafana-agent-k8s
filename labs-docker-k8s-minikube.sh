75d99f95a300b64a1e8b370060391935fc9550c7
George Gatheca <gatheca.george@gmail.com>
2023-11-11 03:51:11 +0000

    minikube start fix

--- labs-docker-k8s-minikube.sh
+++ labs-docker-k8s-minikube.sh
@@ -5,4 +5,4 @@ curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-
 sudo install minikube-linux-amd64 /usr/local/bin/minikube 
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl" 
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
-minikube start --extra-config=apiserver.bind-address=0.0.0.0 --force
+minikube start --force
