049a2d63981dab6c757ccafe0e4e7b3394a4e39a
George Gatheca <gatheca.george@gmail.com>
2023-11-11 01:30:16 +0000

    setup single node cluster

--- labs-k8s.sh
+++ labs-k8s.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
-kubeadm init --apiserver-advertise-address $(hostname -i) --pod-network-cidr 10.5.0.0/16
+# kubeadm init --apiserver-advertise-address $(hostname -i) --pod-network-cidr 10.5.0.0/16
 
-kubectl apply -f https://raw.githubusercontent.com/cloudnativelabs/kube-router/master/daemonset/kubeadm-kuberouter.yaml
+# kubectl apply -f https://raw.githubusercontent.com/cloudnativelabs/kube-router/master/daemonset/kubeadm-kuberouter.yaml
 
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/website/master/content/en/examples/application/nginx-app.yaml
+# kubectl apply -f https://raw.githubusercontent.com/kubernetes/website/master/content/en/examples/application/nginx-app.yaml
 # kubectl delete -f https://raw.githubusercontent.com/kubernetes/website/master/content/en/examples/application/nginx-app.yaml
 
-kubeadm join 192.168.0.23:6443 --token ky2d7t.tbmmveok8ilmumnu --discovery-token-ca-cert-hash sha256:e3845e7b704f9b4fa69783cef67b9a28ba2471b764d5a413d51d0020b98e7f14 
+# kubeadm join 192.168.0.23:6443 --token ky2d7t.tbmmveok8ilmumnu --discovery-token-ca-cert-hash sha256:e3845e7b704f9b4fa69783cef67b9a28ba2471b764d5a413d51d0020b98e7f14 
+
+kubeadm init --pod-network-cidr=10.244.0.0/16
+mkdir -p $HOME/.kube
+cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+chown $(id -u):$(id -g) $HOME/.kube/config
+kubectl taint nodes --all node-role.kubernetes.io/master-
+kubectl taint nodes --all node-role.kubernetes.io/control-plane-
+kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
+curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
+helm repo add openebs https://openebs.github.io/charts
+kubectl create namespace openebs
+helm --namespace=openebs install openebs openebs/openebs
